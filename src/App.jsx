4aec3b1b80837647658bf84ed9723dfa614f20f4
S DURGESH REDDY <76164316+SDurgeshReddy@users.noreply.github.com>
2024-09-07 22:42:58 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/Newsboard"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBoard/>
+    </div>
+  )
+}
+
+export default App
